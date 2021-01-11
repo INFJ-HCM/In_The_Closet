2d768c7da665249f473e732cc9356125005dc301
HYUNA PARK <43696800+gaebom2@users.noreply.github.com>
2021-01-11 21:18:08 +0900

    튜토리얼 앱 첫 실행시에만 띄우기

--- app/src/main/java/com/example/android/butcher2/Loading.java
+++ app/src/main/java/com/example/android/butcher2/Loading.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
@@ -15,8 +16,10 @@ import com.bumptech.glide.request.target.DrawableImageViewTarget;
 import java.time.Instant;
 
 public class Loading extends Activity {
+    public SharedPreferences prefs; //선언
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        prefs = getSharedPreferences("Pref", MODE_PRIVATE); //생성
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
         ImageView splashGif = (ImageView)findViewById(R.id.loading);
@@ -25,15 +28,31 @@ public class Loading extends Activity {
         Glide.with(this).load(R.raw.loading).into(splashGif);
         startLoading();
     }
+
+    //20210111 박현아
     private void startLoading(){
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(getApplicationContext(),TutorialActivity.class);
-                startActivity(intent);
-                finish();
+                boolean isFirstRun = prefs.getBoolean("isFirstRun", true);
+
+                if(isFirstRun) {
+                    Intent intent = new Intent(getApplicationContext(), TutorialActivity.class);
+                    startActivity(intent);
+                    //finish();
+                    prefs.edit().putBoolean("isFirstRun",false).apply();
+                }
+
+                else{
+                    Intent intent2 = new Intent(getApplicationContext(), StartActivity.class);
+                    startActivity(intent2);
+                    finish();
+                }
             }
         },3000);
     }
+
+
+
 }
